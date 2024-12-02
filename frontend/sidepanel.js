00e6a7947545f57d34790553eec6f9a7968cda4c
Sahil Sharma <vlsharma713@gmail.com>
2024-12-03 00:02:41 +0530

    Update extension sidepanel

--- frontend/sidepanel.js
+++ frontend/sidepanel.js
@@ -1,12 +1,29 @@
+const knowledgeBase = document.querySelector("#knowledge-base");
+let { context } = chrome.storage.local.get("context");
+if (context) {
+  knowledgeBase.value = context;
+}
 document
-  .getElementById("extract-btn")
+  .querySelector("#context-save-btn")
+  .addEventListener("click", async function () {
+    context = knowledgeBase.value;
+    if (context) {
+      chrome.storage.local.set({ context });
+      console.log("Context saved: ", context);
+    } else {
+      console.log("No context found");
+    }
+  });
+
+document
+  .querySelector("#extract-btn") // TODO: Change name from extract btn
   .addEventListener("click", async function () {
     const [tab] = await chrome.tabs.query({
       active: true,
       currentWindow: true,
     });
 
-    console.log("Extracting questions on tab", tab.url);
+    console.log("Extracting questions from tab", tab.url);
     if (tab) {
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
