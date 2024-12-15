58417428bce7c0c96fb7a5a0b75dbb17f4f2e85e
Sahil Sharma <vlsharma713@gmail.com>
2024-12-15 17:18:26 +0530

    Remove torch gpu
    Remove torch cuda support in favour of light docker images. Also it
    doesn't affect performance on inference

--- backend/utils.py
+++ backend/utils.py
@@ -1,10 +1,6 @@
-import torch
 from transformers import pipeline
 
 model_name = "deepset/roberta-base-squad2"
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Use pretrained model for question answering
-model = pipeline(
-    "question-answering", model=model_name, tokenizer=model_name, device=device
-)
+model = pipeline("question-answering", model=model_name, tokenizer=model_name)
